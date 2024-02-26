aa7c49ef756c1b6748594a707b1b48f6e203a387
RenatoXD4 <re.chicahual@duocuc.cl>
2024-02-26 17:15:29 -0300

    Se modificaron los archivos para que estos puedan usar SQLAlchemy y se creó un CHANGE LOG para documentar los cambios realizados

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,15 @@
 from flask import Flask
-from flask_mysqldb import MySQL  # Importa MySQL desde flask_mysql
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    # Configuración de la aplicación
-    app.config.from_object(Config)
+db = SQLAlchemy(app)
 
-    # Configuración de MySQL con Flask-MySQLdb
-    mysql = MySQL(app)
-
-    # Importa y registra tus blueprints aquí
-    from main.views import main_bp
-    app.register_blueprint(main_bp)
-
-    return app
+# Importa y registra tus blueprints aquí
+from app.main.views import main_bp
+app.register_blueprint(main_bp)
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
