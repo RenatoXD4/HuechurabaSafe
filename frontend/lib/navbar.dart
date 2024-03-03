003d46550575abeb378a8af5a4c3a6441700181d
RenatoXD4 <re.chicahual@duocuc.cl>
2024-03-02 22:08:48 -0300

    Saqué el lib del gitIgnore e hice el navbar por ahora.
    El botón del navbar tiene que funcionar, voy a investigar cómo se puede hacer una barra lateral y el diseño del nav está por ahora hecho

---
+++ frontend/lib/navbar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class NavBar extends StatelessWidget{
+  const NavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56,
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      decoration: BoxDecoration(color: Colors.amber.shade600),
+      child:const Row(children: [
+            IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'Menú de navegación',
+              onPressed: null,
+            ),
+            SizedBox(width: 8,),
+            Expanded(
+              child: Image(
+              image:AssetImage('assets/huechuraba.png'), height: 36, width: 196, alignment: Alignment.topLeft,)
+              )
+      ],),
+    );
+  }
+}
