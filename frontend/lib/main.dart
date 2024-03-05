8d1fb10f205a0629e7b5fb0ac8afebf972d3973e
RenatoXD4 <re.chicahual@duocuc.cl>
2024-03-05 00:00:54 -0300

    Hice una parte del perfil del conductor
    Logré diseñar la parte del perfil del conductor. La imagen específicamente por ahora

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -8,16 +8,53 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  @override
+   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-        appBar: NavBar(),
+          appBar: const NavBar(),
+          body: Container(
+            height: 180.0,
+            width: MediaQuery.of(context).size.width,
+            decoration: _boxShadow(), // Fondo del contenedor
+            child: OverflowBox(
+              maxHeight: double.infinity,
+              child: Container(
+                margin: const EdgeInsets.only(top: 70.0),
+                child: SizedBox(
+                  height: 280.0,
+                  width: 150.0,
+                  child: CircleAvatar(
+                    radius: 56,
+                    backgroundColor: Colors.amber.shade600,
+                    child: Padding(
+                      padding: const EdgeInsets.all(25), // Border radius
+                      child: ClipOval(
+                        child: Image.asset('assets/persona.png'),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
+
+  BoxDecoration _boxShadow() {
+    return BoxDecoration(
+      color: Colors.amber.shade600, // Fondo del contenedor
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.5), // Color de la sombra
+          spreadRadius: 2.0, // Propagación de la sombra
+          blurRadius: 5.0, // Desenfoque de la sombra
+          offset: const Offset(0, 2), // Desplazamiento de la sombra
+        ),
+      ],
+    );
+  }
 }
-
-
