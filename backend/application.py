bb74fae778aa542a71c2b85006431e6fd7b65e68
RenatoXD4 <re.chicahual@duocuc.cl>
2024-03-24 22:23:55 -0300

    Ahora se migran los modelos

--- backend/application.py
+++ backend/application.py
@@ -6,17 +6,22 @@ from app.main.views import main_bp
 from app.usuario.views import usuario_bp
 from app.rol.views import rol_bp
 
+
+from app.models import Usuario, Rol, Conductor
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db.init_app(app)
-migrate = Migrate(app, db)
 
 app.register_blueprint(main_bp)
 
 app.register_blueprint(usuario_bp)
 
 app.register_blueprint(rol_bp)
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
 
 
 if __name__ == "__main__":
--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = '4e908bbd06d8918afef7c1d6c1a4ef56'
-    SQLALCHEMY_DATABASE_URI = 'mariadb://root:huechuraba@127.0.0.1/huechuraba'
+    SQLALCHEMY_DATABASE_URI = 'mariadb://root:huechuraba@127.0.0.1/huechurabasafe'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
