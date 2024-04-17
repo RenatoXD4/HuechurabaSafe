5f5e1eb225cd4a584132f3cf6447d6aa8809e298
RenatoXD4 <re.chicahual@duocuc.cl>
2024-04-16 22:11:54 -0400

    Creación de Roles

--- backend/application.py
+++ backend/application.py
@@ -25,7 +25,16 @@ app.register_blueprint(reporte_bp)
 db.init_app(app)
 migrate = Migrate(app, db)
 
+with app.app_context():
+    if not Rol.query.filter_by(nombre_rol='Usuario').first():
+        rol_usuario = Rol(nombre_rol='Usuario')
+        db.session.add(rol_usuario)
 
+    if not Rol.query.filter_by(nombre_rol='Administrador').first():
+        rol_administrador = Rol(nombre_rol='Administrador')
+        db.session.add(rol_administrador)
+
+    db.session.commit()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
