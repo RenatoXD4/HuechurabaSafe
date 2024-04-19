0ec72f98eb971e2d5b2cc4adbf98f9c512287f3c
RenatoXD4 <re.chicahual@duocuc.cl>
2024-04-19 14:54:31 -0400

    Update application.py

--- backend/application.py
+++ backend/application.py
@@ -44,8 +44,7 @@ with app.app_context():
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
+    return f"Hello world!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 9090)))
