d3660580b68bcfbec124b7251fc8b38ea7f09bc5
RenatoXD4 <re.chicahual@duocuc.cl>
2024-04-23 18:30:02 -0400

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,3 +2,4 @@ class Config:
     SECRET_KEY = '4e908bbd06d8918afef7c1d6c1a4ef56'
     SQLALCHEMY_DATABASE_URI = 'mysql://prueba1:admin@0.0.0.0/prueba'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024
