643e9f683ea872f1cb90692dae4333d795866716
RenatoXD4 <re.chicahual@duocuc.cl>
2024-05-08 02:02:37 -0400

    Update auth_service.dart

--- frontend/lib/services/auth_service.dart
+++ frontend/lib/services/auth_service.dart
@@ -4,36 +4,65 @@ import 'package:frontend/services/toast_service.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import '../models/usuario_class.dart';
+
 
 class AuthService{
     static Future<String?> login({required String email, required String password}) async {
-    try {
-      final response = await http.post(
-        Uri.parse('http://$apiIp:9090/api/login'),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode(<String, String>{
-          'email': email,
-          'password': password,
-        }),
-      );
+      try {
+        final response = await http.post(
+          Uri.parse('http://$apiIp:9090/api/login'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: jsonEncode(<String, String>{
+            'email': email,
+            'password': password,
+          }),
+        );
 
-      if (response.statusCode == 200) {
-        ToastService.toastService('Inicio de sesión exitoso', const Color.fromARGB(255, 17, 255, 0));
-        return null;
-      } else if (response.statusCode == 401) {
-        // Nombre de usuario o contraseña incorrectos
-        return 'Nombre de usuario o contraseña incorrectos';
-      } else {
-        // Otro error
-        return 'Error desconocido';
+        if (response.statusCode == 200) {
+
+          final sessionId = response.headers['session-id'];
+
+
+          final userResponse = await http.get(
+            Uri.parse('http://$apiIp:9090/api/usuario'),
+            headers: <String, String>{
+              'session-id': sessionId!,
+            },
+          );
+
+          if (userResponse.statusCode == 200) {
+
+            final userData = json.decode(userResponse.body);
+
+            final usuario = Usuario(
+              id: userData['id'],
+              username: userData['username'],
+              email: userData['email'],
+              password: '', // Recuerda, no estás enviando ni recibiendo la contraseña en texto plano
+              rolId: userData['rol_id'], // Asegúrate de usar el nombre correcto del campo de rol según tu modelo
+            );
+
+            
+            return null;
+          } else {
+            // Manejar errores al obtener los detalles del usuario
+            return 'Error al obtener detalles del usuario';
+          }
+        } else if (response.statusCode == 401) {
+          // Nombre de usuario o contraseña incorrectos
+          return 'Nombre de usuario o contraseña incorrectos';
+        } else {
+          // Otro error
+          return 'Error desconocido';
+        }
+      } catch (e) {
+        // Error de conexión
+        return 'Error de conexión';
       }
-    } catch (e) {
-      // Error de conexión
-      return 'Error de conexión';
     }
-  }
 
   static Future<bool> logout() async {
     try {
