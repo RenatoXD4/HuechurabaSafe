81fafd6546036a677897a494beee2673853a95d7
RenatoXD4 <re.chicahual@duocuc.cl>
2024-06-14 19:06:46 -0400

    Update obtenerConductor.js

--- frontend/.netlify/functions/obtenerConductor.js
+++ frontend/.netlify/functions/obtenerConductor.js
@@ -1,37 +1,47 @@
 import fetch from 'node-fetch';
 
 exports.handler = async (event, context) => {
-    const apiIp = process.env.API_IP;
-    const { patente } = event.queryStringParameters;
-  
-    if (!patente) {
+  const apiIp = process.env.API_IP;
+  const { patente } = event.queryStringParameters;
+
+  if (!patente) {
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({ error: 'No se proporcionó la patente.' }),
+    };
+  }
+
+  try {
+    const response = await fetch(`http://${apiIp}:9090/api/obtenerConductor/${patente}`);
+
+    if (response.status === 200) {
+      const jsonData = await response.json();
       return {
-        statusCode: 400,
-        body: JSON.stringify({ error: 'No se proporcionó la patente.' }),
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify(jsonData),
+      };
+    } else {
+      return {
+        statusCode: response.status,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({ error: 'Hubo un error al obtener el conductor.' }),
       };
     }
-  
-    try {
-      const response = await fetch(`http://${apiIp}:9090/api/obtenerConductor/${patente}`, {
-        method: 'GET',
-      });
-  
-      if (response.status === 200) {
-        const jsonData = await response.json();
-        return {
-          statusCode: 200,
-          body: JSON.stringify(jsonData),
-        };
-      } else {
-        return {
-          statusCode: response.status,
-          body: JSON.stringify({ error: 'Hubo un error al obtener el conductor.' }),
-        };
-      }
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: `Hubo un error al obtener el conductor: ${error.message}` }),
-      };
-    }
-  };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({ error: `Hubo un error al obtener el conductor: ${error.message}` }),
+    };
+  }
+};
