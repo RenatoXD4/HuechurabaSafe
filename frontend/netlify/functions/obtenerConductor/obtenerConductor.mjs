a199c388cf3450127b03eb883f8b631776eb9f69
RenatoXD4 <re.chicahual@duocuc.cl>
2024-06-14 20:01:18 -0400

    Node

--- frontend/netlify/functions/obtenerConductor/obtenerConductor.mjs
+++ frontend/netlify/functions/obtenerConductor/obtenerConductor.mjs
@@ -1,53 +1,55 @@
 import fetch from 'node-fetch';
 
 const handler = async (event) => {
-  try {
-    const apiIp = process.env.API_IP;
-    const { patente } = event.queryStringParameters;
-    const headers = {
-      'Access-Control-Allow-Origin': 'https://666cd6883d4ad0330faa68ba--astounding-sprinkles-f47c1e.netlify.app',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+  const apiIp = process.env.API_IP;
+  const { patente } = event.queryStringParameters;
+
+  if (!patente) {
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*',  // O ajusta esto según tu necesidad
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+      },
+      body: JSON.stringify({ error: 'No se proporcionó la patente.' }),
     };
+  }
 
-    if (!patente) {
+  try {
+    const response = await fetch(`http://${apiIp}:9090/api/obtenerConductor/${patente}`);
+
+    if (response.status === 200) {
+      const jsonData = await response.json();
       return {
-        statusCode: 400,
-        headers,
-        body: JSON.stringify({ error: 'No se proporcionó la patente.' }),
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',  // O ajusta esto según tu necesidad
+          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        },
+        body: JSON.stringify(jsonData),
+      };
+    } else {
+      return {
+        statusCode: response.status,
+        headers: {
+          'Access-Control-Allow-Origin': '*',  // O ajusta esto según tu necesidad
+          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        },
+        body: JSON.stringify({ error: 'Hubo un error al obtener el conductor.' }),
       };
     }
-
-    try {
-      const response = await fetch(`http://${apiIp}:9090/api/obtenerConductor/${patente}`);
-
-      if (response.status === 200) {
-        const jsonData = await response.json();
-        return {
-          statusCode: 200,
-          headers,
-          body: JSON.stringify(jsonData),
-        };
-      } else {
-        return {
-          statusCode: response.status,
-          headers,
-          body: JSON.stringify({ error: 'Hubo un error al obtener el conductor.' }),
-        };
-      }
-    } catch (error) {
-      return {
-        statusCode: 500,
-        headers,
-        body: JSON.stringify({ error: `Hubo un error al obtener el conductor: ${error.message}` }),
-      };
-    }
-
   } catch (error) {
     return {
       statusCode: 500,
-      headers,
-      body: JSON.stringify({ error: `Hubo un error: ${error.message}` }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',  // O ajusta esto según tu necesidad
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+      },
+      body: JSON.stringify({ error: `Hubo un error al obtener el conductor: ${error.message}` }),
     };
   }
 };
